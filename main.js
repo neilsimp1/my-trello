9fef0974e3cc6b74b55b373a13beeefeb5c2a702
Neil Simpson <neilsimp1@gmail.com>
2019-02-23 13:31:55 -0500

    Add icons, title, tray

--- main.js
+++ main.js
@@ -1,32 +1,53 @@
-const electron = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow, Menu, Tray } = require('electron');
 
-const path = require("path");
-const url = require("url");
+const path = require('path');
+const url = require('url');
 
 let mainWindow;
+let tray;
 
 function createWindow() {
-	mainWindow = new BrowserWindow({width: 800, height: 600});
+	mainWindow = new BrowserWindow({ 
+		width: 800,
+		height: 600,
+		title: 'Trello',
+		icon: 'trello.png'
+	});
+	mainWindow.setMenu(null);
+
+	tray = new Tray('trello.png');
+	tray.setToolTip('Trello');
+	tray.setContextMenu(Menu.buildFromTemplate([
+		{
+			label: 'Show',
+			click: () => { mainWindow.restore(); }
+		},
+		{
+			label: 'Exit',
+			click: () => {
+				app.isQuiting = true;
+				app.quit();
+			}
+		}
+	]));
 
 	mainWindow.loadURL(url.format({
-		pathname: path.join(__dirname, "index.html"),
-		protocol: "file:",
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
 		slashes: true
 	}));
 
-	mainWindow.on("closed", function() {
+	mainWindow.on('closed', () => {
 		mainWindow = null;
 	});
 }
 
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
-app.on("window-all-closed", function() {
-	if(process.platform !== "darwin") app.quit();
+app.on('window-all-closed', () => {
+	if(process.platform !== 'darwin') app.quit();
 });
 
-app.on("activate", function() {
+app.on('activate', () => {
   if(mainWindow === null) createWindow();
 });
